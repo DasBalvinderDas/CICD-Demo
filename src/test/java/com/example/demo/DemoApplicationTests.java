98b51abfa18ced6b4ec186a1b72b788daed7150b
Das Balvinder Das <balvinder@Balvinder.local>
2021-09-08 17:31:22 +0530

    test cases updated

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,67 @@
 package com.example.demo;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DemoApplicationTests {
+	
 
 	@Test
 	void contextLoads() {
 	}
+	
+	
+	@Test
+	public void testTruncateAInFirst2Positions_AinFirst2Positions() {
+		assertEquals("CD", truncateAInFirst2Positions("AACD"));
+	}
+
+	@Test
+	public void testTruncateAInFirst2Positions_AinFirstPosition() {
+		assertEquals("CD", truncateAInFirst2Positions("ACD"));
+	}
+
+	// ABCD => false, ABAB => true, AB => true, A => false
+	@Test
+	public void testAreFirstAndLastTwoCharactersTheSame_BasicNegativeScenario() {
+		assertFalse( 
+				areFirstAndLastTwoCharactersTheSame("ABCD"));
+	}
+
+	@Test
+	public void testAreFirstAndLastTwoCharactersTheSame_BasicPositiveScenario() {
+		assertTrue( 
+				areFirstAndLastTwoCharactersTheSame("ABAB"));
+	}
+	
+	public String truncateAInFirst2Positions(String str) {
+		if (str.length() <= 2)
+			return str.replaceAll("A", "");
+
+		String first2Chars = str.substring(0, 2);
+		String stringMinusFirst2Chars = str.substring(2);
+
+		return first2Chars.replaceAll("A", "") + stringMinusFirst2Chars;
+	}
+	
+	public boolean areFirstAndLastTwoCharactersTheSame(String str) {
+
+		if (str.length() <= 1)
+			return false;
+		if (str.length() == 2)
+			return true;
+
+		String first2Chars = str.substring(0, 2);
+
+		String last2Chars = str.substring(str.length() - 2);
+
+		return first2Chars.equals(last2Chars);
+	}
+
 
 }
